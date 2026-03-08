47fe84b420f3c300b162235157bcf920dc3fe462
kolubik <kolubakindmitrij@gmail.com>
2026-03-08 11:45:41 +0200

    Update __init__.py

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_class=Config):
     from app.routes.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    from main import bp as main_bp
+    from app.routes.main import bp as main_bp
     app.register_blueprint(main_bp)
 
     from app.routes.admin import bp as admin_bp
